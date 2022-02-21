a85ecd8dab9ef119dc4fa32534e0217efaef6852
Koushik <koushikrp26@gmail.com>
2022-02-22 01:53:52 +0530

    feat: added usage,getting-started,typography pages

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const responsiveSideBar = document.querySelector(".respSidebar");
+
+const closeNavBar = () => {
+	responsiveSideBar.style.display = "none";
+};
+
+const changeHam = () => {
+	responsiveSideBar.style.display = "flex";
+};
